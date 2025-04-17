825b19d624e5dbf089d08598ae4964191430d622
Achyuth_Kumar <achyuthkumar0010@gmail.com>
2025-04-16 23:14:50 -0400

    new class added

---
+++ src/main/java/testJava/SampleDoc.java
@@ -0,0 +1,9 @@
+package testJava;
+
+public class SampleDoc {
+    public static void main(String[] args) {
+        if (5 == 5) {
+            System.out.println("both are equal");
+        }
+    }
+}
