6be10c7e11ab12e9652cf1bb8ae0622ed65cd2db
Achyuth_Kumar <achyuthkumar0010@gmail.com>
2025-04-16 23:17:07 -0400

    updated before class

--- src/main/java/testJava/SampleDoc.java
+++ src/main/java/testJava/SampleDoc.java
@@ -4,6 +4,8 @@ public class SampleDoc {
     public static void main(String[] args) {
         if (5 == 5) {
             System.out.println("both are equal");
+        } else {
+            System.out.println("Both are not equal");
         }
     }
 }
