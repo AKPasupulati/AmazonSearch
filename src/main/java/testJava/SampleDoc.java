43075112404a44fc4a977ed219a6d688319165a7
Achyuth_Kumar <achyuthkumar0010@gmail.com>
2025-04-16 23:30:29 -0400

    this is committing to master branch

---
+++ src/main/java/testJava/SampleDoc.java
@@ -0,0 +1,12 @@
+package testJava;
+
+public class SampleDoc {
+    public static void main(String[] args) {
+        if (5 == 5) {
+            System.out.println("both are equal");
+        } else {
+            System.out.println("Both are not equal");
+        }
+        System.out.println("this line will get every time");
+    }
+}
